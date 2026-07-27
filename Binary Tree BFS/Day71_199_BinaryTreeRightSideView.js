1ab264b89e2dd96e0f0cac90ea5f82c1e07694de
Saumya Kesur <saumyakesur@gmail.com>
2026-07-27 18:09:11 +0530

    Solved Problem 199 From Binary Tree BFS

---
+++ Binary Tree BFS/Day71_199_BinaryTreeRightSideView.js
@@ -0,0 +1,59 @@
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+// Example 1:
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+
+// Example 2:
+// Input: root = [1,2,3,4,null,null,null,5]
+// Output: [1,3,4,5]
+
+// Example 3:
+// Input: root = [1,null,3]
+// Output: [1,3]
+
+// Example 4:
+// Input: root = []
+// Output: []
+
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+var rightSideView = function(root) {
+    if (!root) return [];
+
+    const result = [];
+    const queue = [root];
+
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+
+            if (i === levelSize - 1) {
+                result.push(node.val);
+            }
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+
+    return result;
+};
