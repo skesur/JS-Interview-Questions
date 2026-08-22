da903a94d7470adb8730687eea5cdd27dc215173
Saumya Kesur <saumyakesur@gmail.com>
2026-08-22 21:14:27 +0530

    Solved Problem 211 of Trie

---
+++ Trie/Day85_211_DesignAddAndSearchWordsDataStructure.js
@@ -0,0 +1,82 @@
+// Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+// Implement the WordDictionary class:
+
+// WordDictionary() Initializes the object.
+// void addWord(word) Adds word to the data structure, it can be matched later.
+// bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+
+// Example:
+// Input
+// ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+// [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+// Output
+// [null,null,null,null,false,true,true,true]
+// Explanation
+// WordDictionary wordDictionary = new WordDictionary();
+// wordDictionary.addWord("bad");
+// wordDictionary.addWord("dad");
+// wordDictionary.addWord("mad");
+// wordDictionary.search("pad"); // return False
+// wordDictionary.search("bad"); // return True
+// wordDictionary.search(".ad"); // return True
+// wordDictionary.search("b.."); // return True
+
+// Constraints:
+// 1 <= word.length <= 25
+// word in addWord consists of lowercase English letters.
+// word in search consist of '.' or lowercase English letters.
+// There will be at most 2 dots in word for search queries.
+// At most 104 calls will be made to addWord and search.
+
+
+var WordDictionary = function() {
+    this.children = {};
+    this.isEnd = false;
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function(word) {
+    let node = this;
+    for (const ch of word) {
+        if (!node.children[ch]) {
+            node.children[ch] = new WordDictionary();
+        }
+        node = node.children[ch];
+    }
+    node.isEnd = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function(word) {
+    function dfs(node, index) {
+        if (index === word.length) return node.isEnd;
+
+        const ch = word[index];
+
+        if (ch === '.') {   
+            for (const child of Object.values(node.children)) {
+                if (dfs(child, index + 1)) return true;
+            }
+            return false;
+        } else {
+            if (!node.children[ch]) return false;
+            return dfs(node.children[ch], index + 1);
+        }
+    }
+
+    return dfs(this, 0);
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
