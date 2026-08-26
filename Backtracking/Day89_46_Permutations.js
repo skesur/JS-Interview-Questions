74bbfa577fd4f710d4cadfd1d74aa58aa2fd7e34
Saumya Kesur <saumyakesur@gmail.com>
2026-08-26 14:00:17 +0530

    Solved Problem 46 Of Permutations

---
+++ Backtracking/Day89_46_Permutations.js
@@ -0,0 +1,49 @@
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+// Example 2:
+// Input: nums = [0,1]
+// Output: [[0,1],[1,0]]
+
+// Example 3:
+// Input: nums = [1]
+// Output: [[1]]
+
+// Constraints:
+// 1 <= nums.length <= 6
+// -10 <= nums[i] <= 10
+// All the integers of nums are unique.
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    const result = [];
+    const used = new Array(nums.length).fill(false);
+
+    function backtrack(current) {
+        if (current.length === nums.length) {
+            result.push([...current]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            current.push(nums[i]);
+
+            backtrack(current);
+
+            current.pop();
+            used[i] = false;
+        }
+    }
+
+    backtrack([]);
+    return result;
+};
