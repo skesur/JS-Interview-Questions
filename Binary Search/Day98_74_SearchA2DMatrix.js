0f660b36c2dfbf341c1ed659a1a9af76e1638a9e
Saumya Kesur <saumyakesur@gmail.com>
2026-09-04 09:35:21 +0530

    Solved Problem 74 Of Binary Search

---
+++ Binary Search/Day98_74_SearchA2DMatrix.js
@@ -0,0 +1,47 @@
+// You are given an m x n integer matrix matrix with the following two properties:
+
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+// Example 1:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+
+// Example 2:
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+
+// Constraints:
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -104 <= matrix[i][j], target <= 104
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    let left = 0;
+    let right = m * n - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const row = Math.floor(mid / n);
+        const col = mid % n;
+        const val = matrix[row][col];
+
+        if (val === target) return true;
+        else if (val < target) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return false;
+};
