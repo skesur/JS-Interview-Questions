81cea973b75bd4546baba00bb4dc51dbaea8be34
Saumya Kesur <saumyakesur@gmail.com>
2026-09-20 17:10:18 +0530

    Solved Problem 5 Of Multidimensional DP

---
+++ Multidimensional DP/Day114_5_LongestPalindromicSubstring.js
@@ -0,0 +1,41 @@
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+
+// Example 2:
+// Input: s = "cbbd"
+// Output: "bb"
+
+// Constraints:
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let start = 0;
+    let maxLen = 1;
+
+    function expand(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            if (right - left + 1 > maxLen) {
+                maxLen = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        expand(i, i);
+        expand(i, i + 1);
+    }
+
+    return s.substring(start, start + maxLen);
+};
